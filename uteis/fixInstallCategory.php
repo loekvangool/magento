19d962de5eaddd76ed92174bfced2d9cd820b816
Jose Eduardo de Almeida Junior <joeduardo_almeidajr@hotmail.com>
2018-05-16 11:13:16 -0300

    [ANYMARKET] ADD SCRIPT TO FIX INSTALL CATEGORY

---
+++ uteis/fixInstallCategory.php
@@ -0,0 +1,15 @@
+<?php
+require_once('app/Mage.php');
+Mage::app();
+
+$installer = new Mage_Eav_Model_Entity_Setup('core_setup');
+$installer->startSetup();
+
+$entityTypeId = $installer->getEntityTypeId('catalog_product');
+$idAttributeOldSelect = $installer->getAttribute($entityTypeId, 'categoria_anymarket', 'attribute_id');
+$installer->updateAttribute($entityTypeId, $idAttributeOldSelect, 'frontend_input','select');
+$installer->updateAttribute($entityTypeId, $idAttributeOldSelect, 'backend_type','varchar');
+$installer->updateAttribute($entityTypeId, $idAttributeOldSelect, 'backend_model','eav/entity_attribute_backend_array');
+$installer->updateAttribute($entityTypeId, $idAttributeOldSelect, 'source_model','db1_anymarket/system_config_source_categories_values');
+$installer->updateAttribute($entityTypeId, $idAttributeOldSelect, 'note','Selecione uma categoria para enviar ao Anymarket');
+$installer->endSetup();
